39629bb6b3598636ef06bd07ed67a204a19c5912
Kajus <kajus303@gmail.com>
2024-08-09 20:06:32 +0300

    Word component added

--- hangman-react/src/App.jsx
+++ hangman-react/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Word from "./components/Word";
 
 function App() {
+  const [selectedWord] = useState("example");
+  const [correctLetters, setCorrectLetters] = useState([]);
+
   return (
     <div className="App">
       <Header />
+      <Word selectedWord={selectedWord} correctLetters={correctLetters} />
     </div>
   );
 }
