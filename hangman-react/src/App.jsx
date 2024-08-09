ef5d76e32f14be980cb83c6bd699c3a259200b5c
Kajus <kajus303@gmail.com>
2024-08-09 20:41:56 +0300

    Stickman component and images added

--- hangman-react/src/App.jsx
+++ hangman-react/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Word from "./components/Word";
+import Stickman from "./components/Stickman";
 
 function App() {
   const [selectedWord] = useState("example");
   const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongGuesses, setWrongGuesses] = useState(0);
 
   return (
     <div className="App">
       <Header />
+      <Stickman wrongGuesses={wrongGuesses} />
       <Word selectedWord={selectedWord} correctLetters={correctLetters} />
     </div>
   );
