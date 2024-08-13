74294cfad589de00f09b0ecee71d47f50aed1a1a
Kajus <kajus303@gmail.com>
2024-08-13 10:08:59 +0300

    GameOverWindow/Header font color changed

--- src/components/GameOverWindow.jsx
+++ src/components/GameOverWindow.jsx
@@ -1,11 +1,18 @@
 import "./GameOverWindow.css";
 
 function GameOverWindow({ gameWon, onRestart }) {
+  const messageClass = gameWon ? "win-message" : "lose-message";
+  const buttonClass = gameWon ? "win-button" : "lose-button";
+
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>{gameWon ? "You Won!" : "Game Over..."}</h2>
-        <button onClick={onRestart}>Restart Game</button>
+        <h2 className={messageClass}>
+          {gameWon ? "You Won!" : "Game Over..."}
+        </h2>
+        <button onClick={onRestart} className={buttonClass}>
+          Restart Game
+        </button>
       </div>
     </div>
   );
